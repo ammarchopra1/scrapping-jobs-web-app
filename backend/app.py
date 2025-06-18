629c43931e78d99a56e61b3b04f321421fc197c8
ammarchopra1 <ammarchopra@outlook.com>
2025-06-18 18:28:15 +0500

    environment variable update

--- backend/app.py
+++ backend/app.py
@@ -3,19 +3,23 @@ from config.db import engine
 from models.job import Base
 from routes.job_routes import job_routes
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
-app.register_blueprint(job_routes,url_prefix='/jobs')
+# Register blueprint for job routes
+app.register_blueprint(job_routes, url_prefix='/jobs')
 
-# Create tables in the database
+# Create all tables in the database (if not already existing)
 Base.metadata.create_all(bind=engine)
 
+# Root endpoint
 @app.route("/")
 def home():
     return "FLASK app is running"
 
 # Entry point
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use env PORT or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
